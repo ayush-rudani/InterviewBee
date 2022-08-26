419aaa845732ae72b18d87370e0ed6cc13efae45
Ayush Rudani <78534043+R-Ayush777@users.noreply.github.com>
2022-08-26 17:49:10 +0000

    M index.js

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ const app = express();
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 app.use((req, res, next) => {
-    res.send('Welcome to Express');
+  res.send('Welcome to Express');
 });
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
